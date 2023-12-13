6cf31592aa3bb583e6f7b0c55e700d10acd5c8bf
FlorentinAdet <adet.florentin@outlook.fr>
2023-12-13 15:34:55 +0100

    Application première version

--- app.py
+++ app.py
@@ -1,63 +1,106 @@
-from flask import Flask, render_template, request, jsonify
+import tkinter as tk
+import json
 
-app = Flask(__name__)
+class Joueur:
+    def __init__(self, pseudo):
+        self.pseudo = pseudo
+        self.nombre_victoires = 0
 
-# Plateau de jeu initial
-board = [['', '', ''], ['', '', ''], ['', '', '']]
+class TableauDeJeu:
+    def __init__(self, taille):
+        self.taille = taille
+        self.tableau = [[' ' for _ in range(taille)] for _ in range(taille)]
 
-@app.route('/')
-def index():
-    return render_template('index.html', board=board)
+    def jouer(self, ligne, colonne, symbole):
+        if self.tableau[ligne][colonne] == ' ':
+            self.tableau[ligne][colonne] = symbole
+            return True
+        return False
 
-@app.route('/make_move', methods=['POST'])
-def make_move():
-    global board
+    def est_rempli(self):
+        for ligne in self.tableau:
+            if ' ' in ligne:
+                return False
+        return True
 
-    # Récupérer les données du mouvement depuis la requête AJAX
-    row = int(request.form['row'])
-    col = int(request.form['col'])
-    player = request.form['player']
+    def check_victoire(self, symbole):
+        # Vérification des lignes
+        for ligne in self.tableau:
+            if all(cell == symbole for cell in ligne):
+                return True
 
-    # Vérifier si la case est libre
-    if board[row][col] == '':
-        board[row][col] = player
+        # Vérification des colonnes
+        for col in range(self.taille):
+            if all(self.tableau[row][col] == symbole for row in range(self.taille)):
+                return True
 
-    # Vérifier s'il y a un gagnant
-    winner = check_winner()
-    if winner:
-        return jsonify({'status': 'win', 'winner': winner})
-    
-    # Vérifier s'il y a un match nul
-    if check_draw():
-        return jsonify({'status': 'draw'})
+        # Vérification des diagonales
+        if all(self.tableau[i][i] == symbole for i in range(self.taille)) or \
+           all(self.tableau[i][self.taille - 1 - i] == symbole for i in range(self.taille)):
+            return True
 
-    return jsonify({'status': 'ok', 'board': board})
+        return False
 
-def check_winner():
-    # Vérifier les lignes, colonnes et diagonales
-    for i in range(3):
-        # Vérifier les lignes
-        if board[i][0] == board[i][1] == board[i][2] != '':
-            return board[i][0]
+class MorpionGUI:
+    def __init__(self, root, joueur1, joueur2, taille):
+        self.root = root
+        self.root.title("Morpion")
 
-        # Vérifier les colonnes
-        if board[0][i] == board[1][i] == board[2][i] != '':
-            return board[0][i]
+        self.joueur1 = joueur1
+        self.joueur2 = joueur2
 
-    # Vérifier les diagonales
-    if board[0][0] == board[1][1] == board[2][2] != '':
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] != '':
-        return board[0][2]
+        self.taille = taille
+        self.tableau = TableauDeJeu(taille)
+        self.symbole_courant = 'X'
 
-    return None
+        self.boutons = [[None for _ in range(taille)] for _ in range(taille)]
 
-def check_draw():
-    # Vérifier s'il y a une case vide
-    for row in board:
-        if '' in row:
-            return False
-    return True
+        for ligne in range(taille):
+            for col in range(taille):
+                self.boutons[ligne][col] = tk.Button(root, text='', font=('normal', 20), width=4, height=2,
+                                                      command=lambda l=ligne, c=col: self.cliquer_case(l, c))
+                self.boutons[ligne][col].grid(row=ligne, column=col)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    def cliquer_case(self, ligne, colonne):
+        if self.tableau.jouer(ligne, colonne, self.symbole_courant):
+            self.boutons[ligne][colonne].config(text=self.symbole_courant)
+
+            if self.tableau.check_victoire(self.symbole_courant):
+                self.afficher_message_victoire()
+                self.enregistrer_scores()
+                self.reinitialiser_partie()
+            elif self.tableau.est_rempli():
+                self.afficher_message_match_nul()
+                self.reinitialiser_partie()
+            else:
+                self.symbole_courant = 'O' if self.symbole_courant == 'X' else 'X'
+
+    def afficher_message_victoire(self):
+        gagnant = self.joueur1.pseudo if self.symbole_courant == 'O' else self.joueur2.pseudo
+        tk.messagebox.showinfo("Partie terminée", f"Le joueur {gagnant} a gagné!")
+
+    def afficher_message_match_nul(self):
+        tk.messagebox.showinfo("Partie terminée", "Match nul!")
+
+    def enregistrer_scores(self):
+        scores = {'joueur1': {'pseudo': self.joueur1.pseudo, 'victoires': self.joueur1.nombre_victoires},
+                  'joueur2': {'pseudo': self.joueur2.pseudo, 'victoires': self.joueur2.nombre_victoires}}
+
+        with open('scores.json', 'w') as fichier_scores:
+            json.dump(scores, fichier_scores, indent=4)
+
+    def reinitialiser_partie(self):
+        self.tableau = TableauDeJeu(self.taille)
+        self.symbole_courant = 'X'
+
+        for ligne in range(self.taille):
+            for col in range(self.taille):
+                self.boutons[ligne][col].config(text='')
+
+# Exemple d'utilisation
+joueur1 = Joueur("Joueur1")
+joueur2 = Joueur("Joueur2")
+
+root = tk.Tk()
+morpion_gui = MorpionGUI(root, joueur1, joueur2, taille=3)
+root.mainloop()
